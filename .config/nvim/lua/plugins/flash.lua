01c421757bd20350bef32b210643d45ffc407f8f
Martin Barnes <contact@martinbarnes.dev>
2025-04-30 16:08:11 +0100

    feat(nvim): flash jump

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,17 @@
+
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	config = function()
+		require("flash").setup({})
+	end,
+	keys = {
+		{
+			"<leader>f",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+		},
+	},
+}
