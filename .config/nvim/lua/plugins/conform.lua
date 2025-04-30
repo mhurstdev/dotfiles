c62bb5e6ad9f287ad628c43640ac8d149e758d73
Martin Hurst <contact@martinhurst.com>
2025-04-30 16:18:06 +0100

    feat(nvim): treesitter

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,8 +3,6 @@ return {
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
-			javascript = { "prettierd", "prettier", stop_after_first = true },
-			typescript = { "prettierd", "prettier", stop_after_first = true },
 		},
 		format_on_save = {
 			timeout_ms = 500,
