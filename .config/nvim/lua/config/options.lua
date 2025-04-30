a3b71840e6caa27e8d44011db42a5c95a4222617
Martin Barnes <contact@martinbarnes.dev>
2025-04-30 15:37:04 +0100

    feat(nvim): keymaps

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,31 @@
+vim.opt.guicursor = ""
+vim.opt.cursorline = true
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.swapfile = false
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.backup = false
+vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
+vim.opt.undofile = true
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append "@-@"
+vim.opt.updatetime = 1000
+vim.opt.mouse = ""
+vim.opt.clipboard = "unnamedplus"
+vim.opt.list = false
+vim.opt.listchars = {
+	eol = "↲",
+	tab = "» ",
+	trail = "·",
+	extends = "<",
+	precedes = ">",
+}
