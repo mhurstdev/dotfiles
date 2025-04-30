41092cf2bc8f2c2ad8f8972d6b7b51c422d9bb7f
Martin Hurst <contact@martinhurst.com>
2025-04-30 16:23:52 +0100

    feat(nvim): update oil floating dimensions

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,7 +2,7 @@ return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("oil").setup {
+		require("oil").setup({
 			default_file_explorer = true,
 			columns = {
 				"icon",
@@ -16,7 +16,16 @@ return {
 			view_options = {
 				show_hidden = true,
 			},
-		}
+			float = {
+				padding = 10,
+				max_width = 0,
+				max_height = 0,
+				border = "rounded",
+				win_options = {
+					winblend = 0,
+				},
+			},
+		})
 
 		vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>")
 		vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
