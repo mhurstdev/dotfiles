d8422e84bcf2da454f5b68bfd1530331abde0fd4
Martin Hurst <contact@martinhurst.com>
2025-04-30 16:26:28 +0100

    chore(nvim): format

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_autocmd("textyankpost", {
 	group = vim.api.nvim_create_augroup("mbarnesdev-highlight-yank", { clear = true }),
 	callback = function()
-		vim.highlight.on_yank { timeout = 50 }
+		vim.highlight.on_yank({ timeout = 50 })
 	end,
 })
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,35 @@
 return {
 	"nvim-treesitter/nvim-treesitter-textobjects",
 	dependencies = {
-{
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local configs = require("nvim-treesitter.configs")
+		{
+			"nvim-treesitter/nvim-treesitter",
+			build = ":TSUpdate",
+			config = function()
+				local configs = require("nvim-treesitter.configs")
 
-		configs.setup({
-			ensure_installed = {
-				"lua",
-				"vim",
-				"vimdoc",
-			},
-			auto_install = true,
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
+				configs.setup({
+					ensure_installed = {
+						"lua",
+						"vim",
+						"vimdoc",
+					},
+					auto_install = true,
+					sync_install = false,
+					highlight = { enable = true },
+					indent = { enable = true },
 
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<Enter>", 
-					node_incremental = "<Enter>",
-					scope_incremental = false,
-					node_decremental = "<Backspace>",
-				},
-			},
-		})
-	end,
-
-
-
-        }
+					incremental_selection = {
+						enable = true,
+						keymaps = {
+							init_selection = "<Enter>",
+							node_incremental = "<Enter>",
+							scope_incremental = false,
+							node_decremental = "<Backspace>",
+						},
+					},
+				})
+			end,
+		},
 	},
 	init = function()
 		local config = require("nvim-treesitter.configs")
