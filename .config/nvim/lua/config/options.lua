21797c05134455edceae14bb512229e1259df5d3
Martin Barnes <contact@martinbarnes.dev>
2025-04-30 16:26:28 +0100

    chore(nvim): format

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -10,14 +10,14 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.backup = false
-vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
-vim.opt.isfname:append "@-@"
+vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 1000
 vim.opt.mouse = ""
 vim.opt.clipboard = "unnamedplus"
