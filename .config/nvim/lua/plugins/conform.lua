65770551fb6607f784b843b96c05c36f98c068f1
Martin Barnes <contact@martinbarnes.dev>
2025-05-08 02:39:17 +0100

    feat(nvim): formatter for javascript

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,6 +4,10 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
 			},
 			format_on_save = {
 				timeout_ms = 500,
