9f59e7c11d36aa47f6a6912a5479f2260b7d6baa
Martin Hurst <contact@martinhurst.com>
2025-05-09 12:55:22 +0100

    feat(nvim): lua_ls lsp setup

---
+++ .config/nvim/after/lsp/lua_ls.lua
@@ -0,0 +1,17 @@
+-- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/lua_ls.lua
+
+return {
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
+	root_markers = {
+		".luarc.json",
+		".luarc.jsonc",
+		".luacheckrc",
+		".stylua.toml",
+		"stylua.toml",
+		"selene.toml",
+		"selene.yml",
+		".git",
+	},
+	single_file_support = true,
+}
