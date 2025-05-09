b4ca3f73d500d9e39fca41da76cf98aebb254e6e
mbarnesdev <contact@martinbarnes.dev>
2025-05-09 14:48:28 +0100

    chore(nvim): remove redundant augroup

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,4 @@
 vim.api.nvim_create_autocmd("textyankpost", {
-	group = vim.api.nvim_create_augroup("mbarnesdev-highlight-yank", { clear = true }),
 	callback = function()
 		vim.highlight.on_yank({ timeout = 50 })
 	end,
