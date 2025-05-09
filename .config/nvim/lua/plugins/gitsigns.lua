22459298f00737ca73cd32efb0be14fb27e8d7ed
mbarnesdev <contact@martinbarnes.dev>
2025-05-09 14:48:00 +0100

    chore(nvim): move plugins to opts config when possible

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,14 +1,12 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup({
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "-" },
-				topdelete = { text = "-" },
-				changedelete = { text = "~" },
-			},
-		})
-	end,
+	opts = {
+		signs = {
+			add = { text = "+" },
+			change = { text = "~" },
+			delete = { text = "-" },
+			topdelete = { text = "-" },
+			changedelete = { text = "~" },
+		},
+	},
 }
