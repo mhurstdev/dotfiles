b01d13709c22d0264afd8eba246906b552bcf1c5
Martin Hurst <contact@martinhurst.com>
2025-10-03 19:03:43 +0100

    styles(wezterm): font updatea

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,12 +2,10 @@ local wezterm = require("wezterm")
 
 return {
 	send_composed_key_when_left_alt_is_pressed = true,
-	color_scheme = "Batman",
 	colors = {
 		background = "#000000",
 	},
-	-- font = wezterm.font("Input Mono"),
-	font = wezterm.font("0xProto Nerd Font Mono"),
+	font = wezterm.font("Input Mono"),
 	font_size = 18,
 	harfbuzz_features = {
 		"calt=0",
