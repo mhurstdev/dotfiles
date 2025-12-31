bf45f818b6d1e89ba1d24f09f6750add37d2d18f
Martin Hurst <contact@martinhurst.com>
2025-12-31 01:04:32 +0000

    chore(nvim): remove unused themes

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,63 +1,13 @@
 return {
 	{
-		"EdenEast/nightfox.nvim",
+		"blazkowolf/gruber-darker.nvim",
 		config = function()
-			require("nightfox").setup({
-				options = {
-					transparent = true,
-				},
-			})
+			vim.cmd("colorscheme gruber-darker")
 
-			vim.cmd("colorscheme carbonfox")
-		end,
+			vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+		end
 	},
-	{
-		"ellisonleao/gruvbox.nvim",
-		enabled = false,
-		config = function()
-			require("gruvbox").setup({
-				terminal_colors = true, -- add neovim terminal colors
-				undercurl = true,
-				underline = true,
-				bold = true,
-				italic = {
-					strings = true,
-					emphasis = true,
-					comments = true,
-					operators = false,
-					folds = true,
-				},
-				strikethrough = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				inverse = true, -- invert background for search, diffs, statuslines and errors
-				contrast = "", -- can be "hard", "soft" or empty string
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = false,
-				transparent_mode = true,
-			})
-
-			vim.cmd.colorscheme("gruvbox")
-		end,
-	},
-	-- {
-	-- 	"eldritch-theme/eldritch.nvim",
-	-- 	enabled = false,
-	-- 	config = function()
-	-- 		require("eldritch").setup({
-	-- 			transparent = true,
-	-- 			terminal_colors = true,
-	-- 			styles = {
-	-- 				sidebars = "dark",
-	-- 				floats = "dark",
-	-- 			},
-	-- 		})
-	--
-	-- 		vim.cmd.colorscheme("eldritch")
-	-- 	end,
-	-- },
 	-- {
 	-- 	"Mofiqul/vscode.nvim",
 	-- 	enabled = true,
