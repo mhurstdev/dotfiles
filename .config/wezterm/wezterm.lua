cd0b48bdee285018a61e860aef3ec607622badad
Martin Hurst <contact@martinhurst.com>
2026-04-24 17:22:05 +0100

    feat: add wezterm base config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+return {
+	send_composed_key_when_left_alt_is_pressed = true,
+	colors = {
+		background = "#000000",
+	},
+	color_scheme = "Batman",
+	font = wezterm.font("FiraMono Nerd Font"),
+	font_size = 20,
+	harfbuzz_features = {
+		"calt=0",
+		"clig=0",
+		"liga=0",
+	},
+}
