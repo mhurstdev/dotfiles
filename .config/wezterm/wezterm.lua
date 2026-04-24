89bdb8d1f5aac80147f21076fbef91c14ba34c69
Martin Hurst <contact@martinhurst.com>
2026-04-24 17:32:40 +0100

    feat: add war10ck wezterm color scheme placeholder

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,16 @@
 local wezterm = require("wezterm")
 
-return {
-	send_composed_key_when_left_alt_is_pressed = true,
-	colors = {
-		background = "#000000",
-	},
-	color_scheme = "Batman",
-	font = wezterm.font("FiraMono Nerd Font"),
-	font_size = 20,
-	harfbuzz_features = {
-		"calt=0",
-		"clig=0",
-		"liga=0",
-	},
+local config = wezterm.config_builder()
+
+config.color_schemes = { ['war10ck'] = require("colors.war10ck") }
+config.color_scheme = "war10ck"
+config.send_composed_key_when_left_alt_is_pressed = true
+config.font = wezterm.font("FiraMono Nerd Font")
+config.font_size = 20
+config.harfbuzz_features = {
+	"calt=0",
+	"clig=0",
+	"liga=0",
 }
+
+return config
