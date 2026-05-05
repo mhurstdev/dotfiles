6d8cba17106f15f3ba173ddad4bc6cc9134ac863
Martin Hurst <contact@martinhurst.com>
2026-05-05 11:39:20 +0100

    chore: remove references to war10ck

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,10 +2,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_schemes = { ['war10ck'] = require("colors.war10ck") }
-config.color_scheme = "war10ck"
 config.send_composed_key_when_left_alt_is_pressed = true
-config.font = wezterm.font("FiraMono Nerd Font")
+config.font = wezterm.font("TX-02")
 config.font_size = 20
 config.harfbuzz_features = {
 	"calt=0",
