b8361956743d011c22e3ea269fe76331192d13af
Martin Hurst <contact@martinhurst.com>
2026-05-09 14:42:47 +0100

    chore: adjust development plugin path

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,7 +21,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins_development_path = os.getenv "HOME" .. "/demos"
+local plugins_development_path = os.getenv "HOME" .. "/Developer/demos"
 
 require("lazy").setup("plugins", {
 	dev = {
