322930595acf73b319f1161186f4a064ebce2e25
Martin Hurst <contact@martinhurst.com>
2026-05-09 14:43:10 +0100

    chore: color cursor + format wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,25 @@
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 
 local config = wezterm.config_builder()
 
+-- General
+config.hide_tab_bar_if_only_one_tab = true
 config.send_composed_key_when_left_alt_is_pressed = true
-config.font = wezterm.font("TX-02")
+
+-- Font
+config.font = wezterm.font "TX-02"
 config.font_size = 20
 config.harfbuzz_features = {
 	"calt=0",
 	"clig=0",
 	"liga=0",
+}
+
+-- Colors
+config.colors = {
+	cursor_bg = "#f9e2af",
+	cursor_fg = "#1e1e2e",
+	cursor_border = "#f9e2af",
 }
 
 return config
