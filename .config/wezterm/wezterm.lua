0d75e04f93a49fba73cf844f7971e75f2478b35b
Martin Hurst <contact@martinhurst.com>
2026-05-10 19:51:16 +0100

    chore: add wezterm background

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,6 +6,18 @@ local config = wezterm.config_builder()
 config.hide_tab_bar_if_only_one_tab = true
 config.send_composed_key_when_left_alt_is_pressed = true
 
+-- Background
+--  config.background = {
+-- 	{
+-- 		source = {
+-- 			File = os.getenv "HOME" .. "/dotfiles/.config/wezterm/background.jpg",
+-- 		},
+-- 		hsb = {
+-- 			brightness = 0.075,
+-- 		},
+-- 	},
+-- }
+--
 -- Font
 config.font = wezterm.font "TX-02"
 config.font_size = 20
